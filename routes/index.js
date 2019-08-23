0bd3ae27cc4d3214240111783bb5c68f140bf940
Sorrel Bri <sorrel@SBJ-comp.local>
2019-08-23 09:37:12 -0700

    add authentication routes and authorization middleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const authenticate = require('../middleware/authenticate');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/auth/google', passport.authenticate('google', { scope: [ 'profile', 'email' ] }));
+
+router.get('/oauth2callback', passport.authenticate('google', 
+  { successRedirect: '/main', failureRedirect: '/index' }
+));
+
+router.get('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/index');
+})
+
+router.get('/main', authenticate, (req, res, ) => {
+  // where render of main user page occurs
+})
 
 module.exports = router;
