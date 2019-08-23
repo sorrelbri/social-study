381c0c237c18b014268003245a1702c5cb031a73
Sorrel Bri <sorrel@SBJ-comp.local>
2019-08-23 09:43:36 -0700

    add model structure

--- models/user.js
+++ models/user.js
@@ -1,3 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  avatar: String,
+  googleId: String,
+  bookmarks: [],
+  commments: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Comment'
+  }
+}, {
+  timestamps: true
+});
+
+
+module.exports = mongoose.model('User', userSchema);
