9bbf2f2cb88d9508eaa46d00cf7a88640c0345b9
Sorrel Bri <sorrel@SBJ-comp.local>
2019-08-23 08:39:46 -0700

    initial commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, 
+  { useNewUrlParser: true, useCreateIndex: true }
+);
+
+const db = mongoose.connection;
+
+db.on('connected', () => console.log(`Connected to Mongo DB at ${db.host}:${db.port}`));
---
+++ config/passport.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const User = require('../models/user');
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  }, (accessToken, refreshToken, profile, cb) => {
+    User.findOne( { googleId: profile.id }, (err, user) => {
+      if (err) return cb(err);
+      if (user) {
+        return cb(err, user);
+      } else {
+        let newUser = new user({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          googleId: profile.id,
+          avatar: profile.photos[0].value
+        });
+        newUser.save(err => {
+          return cb(err, newUser);
+        });
+      }
+    });
+  }
+));
