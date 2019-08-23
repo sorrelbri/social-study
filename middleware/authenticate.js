d47bcd7140dc4b96a3c793407e363ee2cfec9ab8
Sorrel Bri <sorrel@SBJ-comp.local>
2019-08-23 15:01:29 -0700

    fix auto-login redirect

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,6 +1,6 @@
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
-  res.redirect('/auth/google');
+  res.redirect('/');
 }
 
 module.exports = isLoggedIn;
--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,23 @@ const authenticate = require('../middleware/authenticate');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: req.user });
+  console.log(req.user)
+  res.render('index', { title: 'Express', user: req.user || null });
 });
 
 router.get('/auth/google', passport.authenticate('google', { scope: [ 'profile', 'email' ] }));
 
 router.get('/oauth2callback', passport.authenticate('google', 
-  { successRedirect: '/main', failureRedirect: '/index' }
+  { successRedirect: '/main', failureRedirect: '/' }
 ));
 
 router.get('/logout', (req, res) => {
   req.logOut();
-  res.redirect('/index');
+  res.redirect('/');
 })
 
 router.get('/main', authenticate, (req, res, ) => {
+  console.log(req.user)
   res.render('main', {user: req.user})
 })
 
